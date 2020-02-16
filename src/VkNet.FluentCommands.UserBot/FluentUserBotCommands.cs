073051606fe4af707476d6575653304931779cc1
Arius Bronte <ariusbronte@gmail.com>
2020-02-16 22:01:55 +0000

    Added message reciever

--- src/VkNet.FluentCommands.UserBot/FluentUserBotCommands.cs
+++ src/VkNet.FluentCommands.UserBot/FluentUserBotCommands.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet.Abstractions;
 using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums.Filters;
 using VkNet.Model;
+using VkNet.Model.RequestParams;
 
 // ReSharper disable MemberCanBeProtected.Global
 
@@ -122,6 +125,112 @@ namespace VkNet.FluentCommands.UserBot
             }
 
             _textCommands.TryAdd(key: (tuple.pattern, tuple.options), value: func);
+        }
+        
+         /// <summary>
+        ///     Starts receiving messages.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        public async Task ReceiveMessageAsync(CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            var longPollServer = await GetLongPollServerAsync(
+                needPts: _longPollConfiguration.NeedPts,
+                lpVersion: _longPollConfiguration.LpVersion,
+                cancellationToken: cancellationToken);
+
+            var pts = longPollServer.Pts;
+            var ts = ulong.Parse(s: longPollServer.Ts);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var longPollHistory = await GetLongPollHistoryAsync(
+                    fields: _longPollConfiguration.Fields,
+                    ts: ts,
+                    pts: pts,
+                    previewLength: _longPollConfiguration.PreviewLength,
+                    onlines: _longPollConfiguration.Onlines,
+                    eventsLimit: _longPollConfiguration.EventsLimit,
+                    msgsLimit: _longPollConfiguration.MsgsLimit,
+                    maxMsgId: _longPollConfiguration.MaxMsgId,
+                    lpVersion: _longPollConfiguration.LpVersion,
+                    cancellationToken: cancellationToken);
+
+                if (longPollHistory?.Messages == null)
+                {
+                    continue;
+                }
+
+                Parallel.ForEach(source: longPollHistory.Messages, body: async update =>
+                {
+                    if (!string.IsNullOrWhiteSpace(update.Text))
+                    {
+                        var command = _textCommands
+                            .Where(predicate: x => 
+                                Regex.IsMatch(input: update.Text, pattern: x.Key.Item1, options: x.Key.Item2))
+                            .Select(selector: x => x.Value)
+                            .SingleOrDefault();
+
+                        if (command == null)
+                        {
+                            return;
+                        }
+
+                        await command(arg1: _botClient, arg2: update, arg3: cancellationToken);
+                    }
+                });
+                
+                pts = longPollHistory.NewPts;
+            }
+        }
+
+        /// <summary>
+        ///     Get data for the connection.
+        /// </summary>
+        /// <param name="needPts">Pts.</param>
+        /// <param name="lpVersion">Version.</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns>Returns data for the connection to long poll</returns>
+        private async Task<LongPollServerResponse> GetLongPollServerAsync(
+            bool needPts,
+            uint lpVersion,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _botClient.Messages.GetLongPollServerAsync(needPts: needPts, lpVersion: lpVersion);
+        }
+
+        /// <summary>
+        ///     Get user events.
+        /// </summary>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        /// <returns>Returns group events.</returns>
+        private async Task<LongPollHistoryResponse> GetLongPollHistoryAsync(
+            UsersFields fields,
+            ulong ts,
+            ulong? pts,
+            long? previewLength,
+            bool? onlines,
+            long? eventsLimit,
+            long? msgsLimit,
+            long? maxMsgId,
+            ulong? lpVersion,
+            CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            return await _botClient.Messages.GetLongPollHistoryAsync(@params: new MessagesGetLongPollHistoryParams
+            {
+                Fields = fields,
+                Ts = ts,
+                Pts = pts,
+                PreviewLength = previewLength,
+                Onlines = onlines,
+                EventsLimit = eventsLimit,
+                MsgsLimit = msgsLimit,
+                MaxMsgId = maxMsgId,
+                LpVersion = lpVersion
+            });
         }
     }
 }
