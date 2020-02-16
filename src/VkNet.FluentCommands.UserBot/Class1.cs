c78f679b22a49632c0f4988b4af8c1e9f47fa38d
Arius Bronte <ariusbronte@gmail.com>
2020-02-16 19:36:50 +0000

    Added FluentUserBotCommands.cs
     Main entry class to use VkNet.FluentCommands.UserBot.

--- src/VkNet.FluentCommands.UserBot/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VkNet.FluentCommands.UserBot
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/VkNet.FluentCommands.UserBot/FluentUserBotCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using VkNet.Abstractions;
+// ReSharper disable MemberCanBeProtected.Global
+
+namespace VkNet.FluentCommands.UserBot
+{
+    /// <inheritdoc />
+    public class FluentUserBotCommands : FluentUserBotCommands<IVkApi>
+    {
+        /// <summary>
+        ///      Initializes a new instance of the <see cref="FluentUserBotCommands"/> class.
+        /// </summary>
+        public FluentUserBotCommands() : base(botClient: () => new VkApi())
+        {
+        }
+    }
+
+    /// <summary>
+    ///     Main entry class to use VkNet.FluentCommands.UserBot.
+    /// </summary>
+    /// <typeparam name="TBotClient">Custom implementation of interaction with VK.</typeparam>
+    public class FluentUserBotCommands<TBotClient> where TBotClient : IVkApi
+    {
+        /// <summary>
+        ///     Implementation of interaction with VK.
+        /// </summary>
+        private readonly TBotClient _botClient;
+
+        /// <summary>
+        ///      Initializes a new instance of the <see cref="FluentUserBotCommands{TBotClient}"/> class.
+        /// </summary>
+        /// <param name="botClient">Implementation of interaction with VK.</param>
+        public FluentUserBotCommands(Func<TBotClient> botClient)
+        {
+            _botClient = botClient();
+        }
+    }
+}
