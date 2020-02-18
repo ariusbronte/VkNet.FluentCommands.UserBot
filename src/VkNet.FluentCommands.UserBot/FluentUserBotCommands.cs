edac615d010f3e2194843efd5caf469495c657ab
Arius Bronte <ariusbronte@gmail.com>
2020-02-18 21:29:01 +0000

    Added the trigger on a text command

--- src/VkNet.FluentCommands.UserBot/FluentUserBotCommands.cs
+++ src/VkNet.FluentCommands.UserBot/FluentUserBotCommands.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using VkNet.Abstractions;
 using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums;
 using VkNet.Enums.Filters;
 using VkNet.Model;
 using VkNet.Model.RequestParams;
@@ -45,8 +46,8 @@ namespace VkNet.FluentCommands.UserBot
         /// <summary>
         ///     Text commands storage.
         /// </summary>
-        private readonly ConcurrentDictionary<(string, RegexOptions), Func<IVkApi, Message, CancellationToken, Task>>
-            _textCommands = new ConcurrentDictionary<(string, RegexOptions), Func<IVkApi, Message, CancellationToken, Task>>();
+        private readonly ConcurrentDictionary<(long?, string, RegexOptions), Func<IVkApi, Message, CancellationToken, Task>>
+            _textCommands = new ConcurrentDictionary<(long?, string, RegexOptions), Func<IVkApi, Message, CancellationToken, Task>>();
 
         /// <summary>
         ///     Stores the message logic exception handler
@@ -89,10 +90,39 @@ namespace VkNet.FluentCommands.UserBot
         {
             OnText(tuple: (pattern, RegexOptions.None), func: func);
         }
+        
+        /// <inheritdoc />
+        public void OnText((string pattern, RegexOptions options) tuple, Func<IVkApi, Message, CancellationToken, Task> func)
+        {
+            OnTextHandler(tuple: (null, tuple.pattern, tuple.options), func: func);
+        }
+        
+        /// <inheritdoc />
+        public void OnText((long peerId, string pattern) tuple, Func<IVkApi, Message, CancellationToken, Task> func)
+        {
+            OnText(tuple: (tuple.peerId, tuple.pattern, RegexOptions.None), func: func);
+        }
 
         /// <inheritdoc />
-        public void OnText((string pattern, RegexOptions options) tuple,
-            Func<IVkApi, Message, CancellationToken, Task> func)
+        public void OnText((long peerId, string pattern, RegexOptions options) tuple, Func<IVkApi, Message, CancellationToken, Task> func)
+        {
+            if (tuple.peerId <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(tuple.peerId));
+            }
+            
+            OnTextHandler(tuple: tuple, func: func);
+        }
+        
+        /// <summary>
+        ///     The main handler for all incoming message triggers
+        /// </summary>
+        /// <param name="tuple">Regular expression and Regex options.</param>
+        /// <param name="func">Trigger actions performed.</param>
+        /// <exception cref="ArgumentException">Thrown if regular expression is null or whitespace.</exception>
+        /// <exception cref="InvalidEnumArgumentException">Thrown if regex options is not defined.</exception>
+        /// <exception cref="ArgumentNullException">Thrown if trigger actions in null.</exception>
+        private void OnTextHandler((long? peerId, string pattern, RegexOptions options) tuple, Func<IVkApi, Message, CancellationToken, Task> func)
         {
             if (string.IsNullOrWhiteSpace(value: tuple.pattern))
             {
@@ -109,7 +139,7 @@ namespace VkNet.FluentCommands.UserBot
                 throw new ArgumentNullException(paramName: nameof(func));
             }
 
-            _textCommands.TryAdd(key: (tuple.pattern, tuple.options), value: func);
+            _textCommands.TryAdd(key: (tuple.peerId, tuple.pattern, tuple.options), value: func);
         }
 
         /// <inheritdoc />
@@ -128,49 +158,42 @@ namespace VkNet.FluentCommands.UserBot
         public async Task ReceiveMessageAsync(CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
-            var longPollServer = await GetLongPollServerAsync(cancellationToken: cancellationToken);
-            
+            var longPollServer = await GetLongPollServerAsync(cancellationToken);
+
             var pts = longPollServer.Pts;
-            var ts = ulong.Parse(s: longPollServer.Ts);
+            var ts = ulong.Parse(longPollServer.Ts);
 
             while (!cancellationToken.IsCancellationRequested)
             {
                 try
                 {
-                    var longPollHistory =
-                        await GetLongPollHistoryAsync(ts: ts, pts: pts, cancellationToken: cancellationToken);
-                    if (longPollHistory?.Messages == null)
+                    var longPollHistory = await GetLongPollHistoryAsync(ts, pts, cancellationToken);
+                    if (!longPollHistory.Messages.Any())
                     {
                         continue;
                     }
 
-                    foreach (var update in longPollHistory.Messages)
+                    foreach (var message in longPollHistory.Messages)
                     {
                         try
                         {
-                            if (!string.IsNullOrWhiteSpace(update.Text))
+                            var type = GetMessageType(message);
+                            switch (type)
                             {
-                                var command = _textCommands
-                                    .Where(predicate: x =>
-                                        Regex.IsMatch(input: update.Text, pattern: x.Key.Item1, options: x.Key.Item2))
-                                    .Select(selector: x => x.Value)
-                                    .SingleOrDefault();
-
-                                if (command == null)
-                                {
-                                    return;
-                                }
-
-                                await command(arg1: _botClient, arg2: update, arg3: cancellationToken);
+                                case MessageType.Message:
+                                    await OnTextMessage(message, cancellationToken);
+                                    break;
+                                case MessageType.None:
+                                    break;
                             }
                         }
                         catch (System.Exception e)
                         {
-                            await (_botException?.Invoke(_botClient, update, e, cancellationToken) ?? throw e);
+                            await (_botException?.Invoke(_botClient, message, e, cancellationToken) ?? throw e);
                         }
-                    }
 
-                    pts = longPollHistory.NewPts;
+                        pts = longPollHistory.NewPts;
+                    }
                 }
                 catch (System.Exception e)
                 {
@@ -179,6 +202,56 @@ namespace VkNet.FluentCommands.UserBot
             }
         }
 
+        /// <summary>
+        ///     This method returns the type of incoming message.
+        /// </summary>
+        /// <param name="message">Private message.</param>
+        /// <returns>The type of incoming message.</returns>
+        private static MessageType GetMessageType(Message message)
+        {
+            if (!string.IsNullOrWhiteSpace(message.Text))
+            {
+                return MessageType.Message;
+            }
+
+            return MessageType.None;
+        }
+        
+        /// <summary>
+        ///     This method has the logic of processing a new message.
+        /// </summary>
+        /// <param name="message">User updates</param>
+        /// <param name="cancellationToken">Propagates notification that operations should be canceled.</param>
+        private async Task OnTextMessage(Message message, CancellationToken cancellationToken = default)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            var command = _textCommands
+                .AsParallel()
+                .Where(x =>
+                {
+                    var peerId = x.Key.Item1;
+                    var pattern = x.Key.Item2;
+                    var options = x.Key.Item3;
+
+                    if (peerId == message.PeerId)
+                    {
+                        return Regex.IsMatch(message.Text, pattern, options);
+                    }
+                    
+                    return !peerId.HasValue && Regex.IsMatch(message.Text, pattern, options);
+                })
+                .Select(x => x.Value)
+                .FirstOrDefault();
+
+            if (command == null)
+            {
+                return;
+            }
+
+            await command(_botClient, message, cancellationToken);
+        }
+        
         /// <summary>
         ///     Get data for the connection.
         /// </summary>
---
+++ src/VkNet.FluentCommands.UserBot/MessageType.cs
@@ -0,0 +1,18 @@
+﻿﻿namespace VkNet.FluentCommands.UserBot
+{
+    /// <summary>
+    ///     The type of incoming message.
+    /// </summary>
+    internal enum MessageType
+    {
+        /// <summary>
+        ///     Unknown type.
+        /// </summary>
+        None,
+        
+        /// <summary>
+        ///     Text message type.
+        /// </summary>
+        Message
+    }
+}
